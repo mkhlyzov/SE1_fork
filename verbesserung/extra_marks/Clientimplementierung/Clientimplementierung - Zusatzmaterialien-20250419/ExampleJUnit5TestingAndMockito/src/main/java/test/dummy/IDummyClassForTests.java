e5d67981c3f99ea6f8285f3b002bb8521e8ab98d
mkhlyzov <s-man911@ukr.net>
2025-07-03 15:54:08 +0300

    fresh start

--- verbesserung/extra_marks/Clientimplementierung/Clientimplementierung - Zusatzmaterialien-20250419/ExampleJUnit5TestingAndMockito/src/main/java/test/dummy/IDummyClassForTests.java
+++
@@ -1,9 +0,0 @@
-package test.dummy;
-
-//used to demonstrate that Mockito can easily create fake implementations (i.e., Mocks)
-//from a given interface (also works for abstracted classes)
-public interface IDummyClassForTests {
-
-	int div(int a, int b);
-
-}
